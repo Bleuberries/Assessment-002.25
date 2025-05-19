7d55bb13e64ea13206cbe3ef5668037256a3af93
Mina <mylastnameiskumai@gmail.com>
2025-05-18 20:44:47 -0400

    update2
    updated playermovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,8 +16,18 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed;
-        rb.MovePosition(rb.position + move * Time.fixedDeltaTime);
+        Vector3 move = new Vector3(moveX, 0, moveZ).normalized;
+
+        if (move.magnitude > 0.01f)
+        {
+            //move the player
+            rb.MovePosition(rb.position + move * Time.fixedDeltaTime);
+
+            //Rotate the player to face movement direction
+            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+            rb.MoveRotation(toRotation);
+        }
+        
     }
 
     void OnTriggerEnter(Collider other)
@@ -28,5 +38,6 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log("Shard collected!");
     }
 }
+    
 
 }
